32d791e353d1d87351a3723e0c4e43b198f3b4c3
Dariusz Rosiek <daro.krk@interia.pl>
2018-03-02 22:53:24 +0100

    Fixed bug in code

--- components/App.js
+++ components/App.js
@@ -19,6 +19,8 @@ App = React.createClass({
 		}.bind(this));
 	},
 	getGif: function(searchingText, callback) {
+		var GIPHY_API_URL = 'https://api.giphy.com';
+    	var GIPHY_PUB_KEY = 'dc6zaTOxFJmzC';
 		var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
 		var xhr = new XMLHttpRequest();
 		xhr.open('GET', url);
