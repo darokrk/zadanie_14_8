84a514b28b068225850efb07cb0448d9a7c88c3b
Dariusz Rosiek <daro.krk@interia.pl>
2018-03-02 15:39:59 +0100

    Added if conditional statement to handleChange function, also added handleKeyUp function

--- components/Search.js
+++ components/Search.js
@@ -9,15 +9,19 @@ Search = React.createClass({
 		this.setState({
 			searchingText: searchingText
 		});
+		if (searchingText.length > 2) {
+			this.props.onSearch(searchingText);
+		}
+	},
+	handleKeyUp: function(event) {
+		if (event.keyCode === 13) {
+			this.props.onSearch(this.state.searchingText);
+		}
 	},
 	render: function() {
-		var styles = {
-			fontSize: '1.5em',
-			width: '90%',
-			maxWidth: '350px'
-		};
+		var styles = {fontSize: '1.5em', width: '90%', maxWidth: '350px'};
 		return (
-			<input type="text" onChange={this.handleChange} placeholder="Enter the search phrase here" 
+			<input type="text" onChange={this.handleChange} onKeyUp={this.handleKeyUp} placeholder="Enter the search phrase here" 
 				style={styles} value={this.state.searchTerm} 
 			/>
 		);
